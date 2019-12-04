1096b876e29ac0f5278cacd8477e83e57d645f12
Vespertino <Vespertino@PC08>
2019-12-04 21:32:20 +0100

    He puesto 4 souts con cosas bonitas

---
+++ src/entornos/Entornos.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entornos;
+
+/**
+ *
+ * @author Vespertino
+ */
+public class Entornos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("olawenas");
+        System.out.println("anvorguesa");
+        System.out.println("miao");
+        System.out.println("hasta luego :o");
+    }
+    
+}
